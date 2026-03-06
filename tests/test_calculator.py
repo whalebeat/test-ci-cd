976a182415d181e6d44e5c2d68f64a1aab613aa1
whalebeat <whalebeat@gmail.com>
2026-03-06 19:37:18 +0700

    Add code and ci/cd

---
+++ tests/test_calculator.py
@@ -0,0 +1,18 @@
+import pytest
+from src.calculator import Calculator
+
+@pytest.fixture
+def calc():
+    return Calculator()
+
+def test_add(calc):
+    assert calc.add(3,5) == 8
+
+def test_subtract(calc):
+    assert calc.subtract(10,4) == 6
+
+def test_multiply(calc):
+    assert calc.multiply(4,5) == 20
+
+def test_divide(calc):
+    assert calc.divide(10,2) == 5
